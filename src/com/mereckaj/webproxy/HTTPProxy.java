18c141353c4556da62b9323ec724b0f9edb30b3f
Julius <mereckaj@tcd.ie>
2015-02-14 19:39:27 +0000

    Cleaned up the HTTPProxy class, added comments

--- src/com/mereckaj/webproxy/HTTPProxy.java
+++ src/com/mereckaj/webproxy/HTTPProxy.java
@@ -6,59 +6,47 @@ import java.util.logging.Level;
 
 import com.mereckaj.webproxy.utils.Utils;
 
+/**
+ * This class creates a new instance of a HTTP Proxy. This class extends thread,
+ * so to run the proxy use run() or start() methods.
+ * 
+ * @author julius
+ * 
+ */
+public class HTTPProxy extends Thread {
+    static ServerSocket socket;
+    static int port;
+    private ProxyDataLogger dataLogger;
+    private ProxyLogger logger;
+    private ProxySettings settings;
 
-public class HTTPProxy extends Thread{
-	public HTTPProxy(){
-		
-	}
-	
-	
-	static ServerSocket socket;
-	
-	
-	static int port;
-	
-	
-	public void run() {
+    /**
+     * Instantiate some of the static resources that will be used
+     * inside this class.
+     */
+    public HTTPProxy() {
+	dataLogger = ProxyDataLogger.getInstance();
+	logger = ProxyLogger.getInstance();
+	settings = ProxySettings.getInstance();
+    }
 
-		
-		port = ProxySettings.getInstance().getProxyPort();
-		
-		try {
-			
-			socket = new ServerSocket(port);
-			
-		} catch (IOException e) {
-			
-			
-			ProxyLogger.getInstance().log(Level.SEVERE,
-					"Couldn't create a new server socket: " + e.getMessage());
-			
-			
-			System.exit(1);
-		}
-		
-		
-		ProxyDataLogger.getInstance().log(Level.INFO, "Started proxy at: " + Utils.getCurrentDate());
-		
-		
-		while (ProxySettings.getInstance().getRunning()) {
-			
-			try {
-				
-				
-				new ProxyWorkerThread(socket.accept()).start();
-			} catch (IOException e) {
-				
-				
-				ProxyLogger
-						.getInstance()
-						.log(Level.WARNING,
-								"Could not create new thread to deal with a new connection");
-			}
-		}
-		
-		
-		System.exit(0);
+    public void run() {
+	port = ProxySettings.getInstance().getProxyPort();
+	try {
+	    socket = new ServerSocket(port);
+	    dataLogger.log(Level.INFO, "Start: " + Utils.getCurrentDate());
+
+	    /*
+	     * Main proxy loop. Each time the socket accepts a connection it
+	     * will span a new worker thread that will deal with it.
+	     */
+	    while (settings.getRunning()) {
+		new ProxyWorkerThread(socket.accept()).start();
+	    }
+
+	} catch (IOException e) {
+	    logger.log(Level.SEVERE, "Error creating proxy" + e.getMessage());
+	    System.exit(1);
 	}
+    }
 }
