f1270ed9524b72d69d289e3582af62d071daf015
Julius <mereckaj@tcd.ie>
2015-02-14 20:33:31 +0000

    Added comments, and cleaned the code a little bit

--- src/com/mereckaj/webproxy/ProxyDataLogger.java
+++ src/com/mereckaj/webproxy/ProxyDataLogger.java
@@ -8,48 +8,80 @@ import java.util.logging.SimpleFormatter;
 
 import com.mereckaj.webproxy.utils.Utils;
 
+/**
+ * This class logs any exceptions or messages that were not related to the
+ * proxying of data<br>
+ * It will log errors and config changes
+ * 
+ * This is a singleton object and the constructor is private.<br>
+ * This means that the only way to get an instance of this object is through
+ * getInstance() method.
+ * 
+ * @author julius
+ * 
+ */
 public class ProxyDataLogger {
-	private static ProxyDataLogger instance = new ProxyDataLogger();
-	private Logger log;
-	private FileHandler fileHandle;
-	private String pathToLog;
-	
-	private ProxyDataLogger() {
-		if (log == null) {
-			log = Logger.getLogger("ProxyDataLog");
-			pathToLog = ProxySettings.getInstance().getPathToLog();
-			try {
-				fileHandle = new FileHandler(pathToLog + "_"
-						+ Utils.getCurrentDate());
-				log.addHandler(fileHandle);
-				fileHandle.setFormatter(new SimpleFormatter());
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		if (!ProxySettings.getInstance().isLoggingEnabled()) {
-			log.setLevel(Level.OFF);
-		}
-	}
+    // Static instance of this object
+    private static ProxyDataLogger instance = new ProxyDataLogger();
 
-	
-	public static ProxyDataLogger getInstance() {
-		return instance;
-	}
+    private Logger log;
+    private FileHandler fileHandle;
+    private String pathToLog;
+    private ProxySettings settings;
 
-	
-	public void log(Level l, String m) {
-		log.log(l, m);
+    private ProxyDataLogger() {
+	if(settings==null){
+	    settings = ProxySettings.getInstance();
 	}
-	public void setEnabled(boolean e){
-	    if(e==false){
-		log.setLevel(Level.OFF);
-	    }else{
-		if(log.getLevel()==Level.OFF){
-		    log.setLevel(Level.ALL);
-		}
+	/*
+	 * If this is the first time the logger is being instantiated
+	 * then create a new file handle to the log file
+	 */
+	if (log == null) {
+	    log = Logger.getLogger("ProxyDataLog");
+	    pathToLog = settings.getPathToLog();
+	    try {
+		fileHandle = new FileHandler(pathToLog + "_" + Utils.getCurrentDate());
+		log.addHandler(fileHandle);
+		fileHandle.setFormatter(new SimpleFormatter());
+	    } catch (SecurityException e) {
+		e.printStackTrace();
+	    } catch (IOException e) {
+		e.printStackTrace();
 	    }
 	}
+	if (!settings.isLoggingEnabled()) {
+	    log.setLevel(Level.OFF);
+	}
+    }
+    
+    /**
+     * This method should be used to obtain an isntance of this class.
+     * @return instance of this class.
+     */
+    public static ProxyDataLogger getInstance() {
+	return instance;
+    }
+    
+    /**
+     * Log a message at a given level
+     * @param l {@link Level} or {@link ProxyLogLevel} at which the message should be logged
+     * @param m message to add to the log
+     */
+    public void log(Level l, String m) {
+	log.log(l, m);
+    }
+    
+    /**
+     * Method used by the GUI to disable or enable logging in real time.
+     */
+    public void setEnabled(boolean e) {
+	if (e == false) {
+	    log.setLevel(Level.OFF);
+	} else {
+	    if (log.getLevel() == Level.OFF) {
+		log.setLevel(Level.ALL);
+	    }
+	}
+    }
 }
